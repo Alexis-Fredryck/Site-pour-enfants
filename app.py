57ae1d929b5f2644440314dd60ac2a46edab6330
Fredryck.Alexis <Alexis.fredryck@outlook.fr>
2025-09-05 16:25:45 +0200

    mise en place de flask

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask 🚀"
+    return render_template("index.html")
+
+@app.route("/iconographies")
+def iconographies():
+    return render_template("Iconographies.html")  # affiche Iconographies.html
 
 if __name__ == "__main__":
     app.run(debug=True)
