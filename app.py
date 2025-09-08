106c06de45564b92b0c05680a7d12b6a351363f6
Fredryck.Alexis <Alexis.fredryck@outlook.fr>
2025-09-08 16:03:05 +0200

    mise en place de ollama

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import requests 
 
 app = Flask(__name__)
 
@@ -8,7 +9,31 @@ def home():
 
 @app.route("/iconographies")
 def iconographies():
-    return render_template("Iconographies.html")  # affiche Iconographies.html
+    return render_template("Iconographies.html") 
+
+
+@app.route("/ask", methods=["GET", "POST"])
+def ask():
+    answer = None
+    if request.method == "POST":
+        question = request.form["question"]
+        
+        # Appel à l'API locale d'Ollama
+        response = requests.post(
+            "http://localhost:11434/api/generate",
+            json={
+                "model": "llama3",
+                "prompt": question,
+                "stream": False
+            }
+        )
+        
+        answer = response.json()["response"]
+
+        return render_template("ask.html", question=question, answer=answer)
+
+    return render_template("ask.html", question=None, answer=None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
